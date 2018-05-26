9d4acfa6664af15e754f3685a80fac9392635994
dubbz82 <whitejeremiah@hotmail.com>
2018-05-26 08:29:44 -0500

    Added version number
    Added version number to form.

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -573,7 +573,7 @@
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Clone Hero INI Editor";
+            this.Text = "Clone Hero INI Editor v 0.4.1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
