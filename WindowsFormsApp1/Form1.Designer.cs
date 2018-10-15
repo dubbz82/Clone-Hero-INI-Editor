778cd0c1e201bf31a9afb4f36e3eb807b78d1fdf
dubbz82 <whitejeremiah@hotmail.com>
2018-10-14 21:46:31 -0500

    version bump on main form.
    meant to check this in with last commit...

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -641,7 +641,7 @@
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Clone Hero INI Editor v 0.8.0";
+            this.Text = "Clone Hero INI Editor v 0.8.1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
